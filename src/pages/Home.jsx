367a277c44b2e27bb4b61a17a1d4083b7990716c
Aditya <anupama12sg@gmail.com>
2025-08-24 19:44:31 +0530

    chore: added Link to all the li items

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import homeLogo from '../assets/home_logo.png'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
@@ -7,14 +8,14 @@ const Home = () => {
             <img src={homeLogo} alt="home page logo" />
             <nav>
                 <ul>
-                    <li>Home</li>
-                    <li>Teacher</li>
-                    <li>Student</li>
-                    <li>About Us</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/teacher">Teacher</Link></li>
+                    <li><Link to="/student">Student</Link></li>
+                    <li><Link to= "/about">About Us</Link></li>
                 </ul>
             </nav>
             <h1>Quiz Mania: Testing you knowledge begins here!</h1>
-            <h3>Finding difficulty in topics like Blockchain Data Structures. Your worry ends here. Practice quizzes and clear your concepts./</h3>
+            <h3>Finding difficulty in topics like Blockchain Data Structures. Your worry ends here. Practice quizzes and clear your concepts.</h3>
         </div>
     )
 }
