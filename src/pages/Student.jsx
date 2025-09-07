1bb3ba221100d7c5ff0cc4f601f30fb992cb6710
Aditya <anupama12sg@gmail.com>
2025-09-07 16:40:28 +0530

    styled student pg navbar

--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -4,12 +4,17 @@ import { Link } from 'react-router-dom'
 
 const Student = () => {
     return (
-        <div>
+        <div className='flex min-h-screen text-gray-900'>
+
+            <aside className='fixed top-0 left-0 h-screen w-64 bg-white shadow-lg z-50 flex flex-col p-6'>
+               <h2 className='text-xl font-bold mb-6'>Quiz Mania</h2>
+                <nav className='flex flex-col space-y-4'>
+                    <ul><Link to="/" className='hover:text-blue-600 font-medium'>Home</Link></ul>
+                    <ul><Link to="/about" className='hover:text-blue-600 font-medium'>About Us</Link></ul>
+                </nav>
+            </aside>
+
             <img src={student} alt="student logo" />
-            <nav>
-                <ul><Link to="/">Home</Link></ul>
-                <ul><Link to= "/about">About Us</Link></ul>
-            </nav>
 
         </div>
     )
