13ef2ed3ae82603b3ebb5692f608778f27616b80
Aditya <anupama12sg@gmail.com>
2025-09-07 16:44:55 +0530

    chore: styled img logo of student pg

--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -7,15 +7,16 @@ const Student = () => {
         <div className='flex min-h-screen text-gray-900'>
 
             <aside className='fixed top-0 left-0 h-screen w-64 bg-white shadow-lg z-50 flex flex-col p-6'>
-               <h2 className='text-xl font-bold mb-6'>Quiz Mania</h2>
+                <h2 className='text-xl font-bold mb-6'>Quiz Mania</h2>
                 <nav className='flex flex-col space-y-4'>
                     <ul><Link to="/" className='hover:text-blue-600 font-medium'>Home</Link></ul>
                     <ul><Link to="/about" className='hover:text-blue-600 font-medium'>About Us</Link></ul>
                 </nav>
             </aside>
 
-            <img src={student} alt="student logo" />
-
+            <main className='flex-1 p-12 flex flex-col items-center justify-center'>
+                <img src={student} alt="student logo" className='w-80 mb-8 rounded-xl shadow-lg' />
+            </main>
         </div>
     )
 }
