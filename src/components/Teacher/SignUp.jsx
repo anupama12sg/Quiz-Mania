3a0ab2b5461af60606e8146a31e05c160e79809b
Aditya <anupama12sg@gmail.com>
2025-09-08 19:31:04 +0530

    wip: login/signup pg

--- src/components/Teacher/SignUp.jsx
+++ src/components/Teacher/SignUp.jsx
@@ -3,37 +3,24 @@ import mail_logo from '../../assets/mail_logo.webp'
 import password from '../../assets/password.png'
 import person from '../../assets/person.png'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const SignUp = () => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
+
     return (
         <>
-        <ul><Link to={"/teacher"}>Teacher</Link></ul>
-         <ul><Link to={"/addquestion"}>Add Question</Link></ul>
-            <div className='signup-container'>
-                <div className='header'>
-                    <div className='text'>Sign Up</div>
-                    <div className='underline'></div>
-                </div>
-                <div className='inputs'>
-                    <div className='input'>
-                        <img src={person} alt="" />
-                        <input type="text" />
-                    </div>
-                    <div className='input'>
-                        <img src={mail_logo} alt="" />
-                        <input type="email" />
-                    </div>
-                    <div className='input'>
-                        <img src={password} alt="" />
-                        <input type="password" />
-                    </div>
-                </div>
-                <div className="forgot-password">Lost password? <span>Click Here!</span></div>
-                <div className="submit-container">
-                    <div className="submit">Sign Up</div>
-                    <div className="submit">Login</div>
-                </div>
-            </div>
+            <ul><Link to={"/teacher"}>Teacher</Link></ul>
+            <ul><Link to={"/addquestion"}>Add Question</Link></ul>
+            <form>
+                <input type="text" placeholder='Enter your name' value={name} onChange={(e) => setName(e.target.value)} />
+                <input type="text" placeholder='Enter your Email ID' value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" placeholder='Enter your Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+            </form>
+
         </>
     )
 }
