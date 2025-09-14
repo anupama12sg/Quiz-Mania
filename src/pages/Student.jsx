d9c2b78f0674ffd6c8acc8a7f630c78901bef9b2
Aditya <anupama12sg@gmail.com>
2025-09-14 13:45:03 +0530

    chore: added some content text to student and teacher pg

--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -16,6 +16,15 @@ const Student = () => {
 
             <main className='flex-1 p-12 flex flex-col items-center justify-center ml-64'>
                 <img src={student} alt="student logo" className='w-80 mb-8 rounded-xl shadow-lg' />
+                <h2 className='text-3xl font-bold mb-4 text-center'>Student</h2>
+                <ul className='list-disc list-inside text-lg text-gray-700 leading-relaxed mb-8 mt-2'>
+                    <p>Students can do the following:</p>
+                    <li> 
+                        <span className='font-medium'>Select a topic & attempt the quiz</span></li>
+                    <li>
+                        <span className='font-medium'>Scores to be stored on- chain which can be accessed by the students</span></li>
+                </ul>
+
             </main>
         </div>
     )
--- src/pages/Teacher.jsx
+++ src/pages/Teacher.jsx
@@ -19,6 +19,10 @@ const Teacher = () => {
             {/*Main Content*/}
             <main className='flex-1 p-12 flex flex-col items-center justify-center ml-64'>
                 <img src={teacher} alt="teacher logo" className='w-80 mb-8 rounded-xl shadow-lg' />
+                <h2 className='text-3xl font-bold mb-4 text-center'>Teacher</h2>
+                <p className='text-lg text-gray-700 leading-relaxed text-justify mb-8'>
+                    Teacher's can add a topic & add Q&A for the same after they login/signup.
+                </p>
             </main>
         </div>
     )
